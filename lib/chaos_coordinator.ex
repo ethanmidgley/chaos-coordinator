532725b7279bfacea5e6d81b7609d210043ebae0
ethanmidgley <ethan.c.midgley@gmail.com>
2025-07-09 20:24:37 +0100

    Example calendar entries to solve

--- lib/chaos_coordinator.ex
+++ lib/chaos_coordinator.ex
@@ -15,4 +15,29 @@ defmodule ChaosCoordinator do
   def hello do
     :world
   end
+
+  def example_events do
+    [
+      %Calendar.Entry{
+        title: "before",
+        # deadline: ~U[2025-07-09 14:00:00Z],
+        fixed_start_time: ~U[2025-07-09 11:00:00Z],
+        dependencies: [],
+        duration: %Duration{minute: 15},
+        note: "",
+        fixed: true,
+        priority: 3
+      },
+      %Calendar.Entry{
+        title: "later",
+        deadline: ~U[2025-07-09 12:00:00Z],
+        fixed_start_time: nil,
+        dependencies: ["before"],
+        duration: %Duration{minute: 45},
+        note: "",
+        fixed: false,
+        priority: 1
+      }
+    ]
+  end
 end
